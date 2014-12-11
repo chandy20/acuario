4d73c34ecf680c298ca051fc82c2a821614dd567
Jhon Alberto Lizcano Parra <betobest27@gmail.com>
2014-12-11 12:19:49 -0500

    no message

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -19,9 +19,9 @@ public class Principal {
      */
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
-        Tactil tactil = new Tactil();
-//        tactil.setLocationRelativeTo(null);
-        tactil.setVisible(true);
+        Inicio inicio = new Inicio();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
     }
 
 }
